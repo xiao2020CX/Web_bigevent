c756a67208db41a73133bdd86aee5e5eb32dc86a
xiao2020 <1184218585@qq.com>
2020-10-12 14:37:28 +0800

    完成了login 页面

--- assets/js/login1.js
+++ assets/js/login1.js
@@ -13,8 +13,10 @@ $(function(){
 
     // 从 Layui 中获取 form 对象
     var form =layui.form
+    var layer = layui.layer
     // 通过form.verify()函数自定义 校验规则
     form.verify({
+        // 自定义了一个叫做 pwd 的校验规则
         pwd: [ /^[\S]{6,12}$/ ,'密码必须6到12位，且不能出现空格'],
         // 校验两次密码是否一致 的规则
         repwd:function(value){
@@ -28,8 +30,42 @@ $(function(){
             }
         }
     });  
-    // 监听注册表单的提交事件
-    $('#form_reg').on('submit',function(e){
-        e.preventDefault()
-    })    
+    
+     // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function(res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
+      $('#link_login').click()
+    })
+  })
+  // 监听登录表单的提交事件
+  $('#foem_login').submit(function(e){
+    // 阻止默认表单提交行为
+    e.preventDefault()
+    $.ajax({
+        url:'',
+        method:'POST',
+        data:$(this).serialize(),success:function(res){
+            if(res.status !==0){
+                return layer.msg('登陆失败')
+            }
+            layer.msg('登陆成功')
+            // 将登录成功得到的 token 字符串，保存到 localStorage 中
+            localStorage.setItem('token',res.token)
+            // 跳转到后台主页
+            location.href='/index.html'
+        }
+    })
+  })
 })
